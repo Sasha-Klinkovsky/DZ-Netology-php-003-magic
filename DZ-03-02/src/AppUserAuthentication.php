e69d91fc55cb7c3415bac37c3624e5888d20772e
Александр Клинковский <150130166+Sasha-Klinkovsky@users.noreply.github.com>
2024-11-01 18:01:08 +0300

    Update AppUserAuthentication.php
    Исправил названия метода на единое authenticate

--- DZ-03-02/src/AppUserAuthentication.php
+++ DZ-03-02/src/AppUserAuthentication.php
@@ -12,7 +12,7 @@ trait AppUserAuthentication {
     private string $appLogin = 'app-Misha';
     private string $appPassword = 'app-Parol';
 
-    public function authenticateApp(string $login, string $password): bool {
+    public function authenticate(string $login, string $password): bool {
         return $login === $this->appLogin && $password === $this->appPassword;
     }
 }
